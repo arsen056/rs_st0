7cc2f08f30bca8125a0b4a913e37074dde882844
arsen056 <vurdihanov@gmail.com>
2022-01-05 21:38:15 +0300

    fix: fix adaptive burger menu, closing on clicking on links

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,6 +1,20 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
+const links =  nav.querySelectorAll('.nav-link');
+
 burger.addEventListener('click', () => {
+    document.body.classList.toggle('lock');
     burger.classList.toggle('burger-active');
     nav.classList.toggle('nav-active');
+
 });
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        if (nav.classList.contains('nav-active')) {
+            document.body.classList.remove('lock');
+            burger.classList.remove('burger-active');
+            nav.classList.remove('nav-active');
+        }
+    })
+})
