afdc10f9133e1de8dac66633ed743569e366f47c
arsen056 <vurdihanov@gmail.com>
2022-01-06 09:52:20 +0300

    feat: add change images in portfolio section

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,4 @@
+// ----- Burger -----
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
 const links =  nav.querySelectorAll('.nav-link');
@@ -16,5 +17,27 @@ function closeMenu() {
         document.body.classList.remove('lock');
         burger.classList.remove('burger-active');
         nav.classList.remove('nav-active');
+    }
+}
+
+// ----- Portfolio img -----
+
+const portfolioBtns = document.querySelector('.button-list');
+const portfolioImages = document.querySelectorAll('.portfolio-img');
+const portfolioButtons = document.querySelectorAll('.portfolio-btn');
+
+portfolioBtns.addEventListener('click', changeImage);
+
+function changeImage(event) {
+    let target = event.target;
+
+    // Delete highlite btn
+    portfolioButtons.forEach(btn => {
+        btn.classList.remove('btn-active')
+    })
+
+    if (event.target.classList.contains('portfolio-btn')) {     
+        event.target.classList.add('btn-active');         
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/${target.dataset.season}/${index + 1}.jpg`);
     }
 }
