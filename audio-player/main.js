1c5a94719d305df3a82f2d3e0b504701fc8cd519
arsen056 <vurdihanov@gmail.com>
2022-02-03 01:13:21 +0300

    feat: add function next and prev music

--- audio-player/main.js
+++ audio-player/main.js
@@ -1,6 +1,25 @@
-const audio = document.querySelector('audio');
+const audio = document.querySelector('audio'),
+      playPause = document.querySelector('.play-toggle'),
+      background = document.querySelector('.background'),
+      cover = document.querySelector('.cover'),
+      singer = document.querySelector('.singer'),
+      next = document.querySelector('.next'),
+      prev = document.querySelector('.prev'),
+      title = document.querySelector('.title-song');
+
 let isPlay = false;
-const playPause = document.querySelector('.play-toggle');
+
+const musics = ["Don't Start Now", "Don't Hurt Yourself"];
+const singers = ["Dua Lipa", "Beyonce"];
+let musicIndex = 0;
+
+function initMusic(music, nameSinger) {
+    audio.src = `assets/audio/${music}.mp3`;
+    singer.innerHTML = nameSinger;
+    title.innerHTML = music;
+    background.style.backgroundImage = `url('./assets/img/cover${musicIndex + 1}.png')`;
+    cover.style.backgroundImage = `url('./assets/img/cover${musicIndex + 1}.png')`;
+}
 
 function playAudio() {
   audio.play();
@@ -22,8 +41,30 @@ function togglePlay() {
     }
 }
 
+// Next music
+
+function nextMusic() {
+    musicIndex++;
+    if (musicIndex > musics.length - 1) {
+        musicIndex = 0;
+    }
+    initMusic(musics[musicIndex], singers[musicIndex]);
+}
+next.addEventListener('click', nextMusic);
+
+// Prev music
+function prevMusic() {
+    musicIndex--;
+    if (musicIndex < 0) {
+        musicIndex = musics.length - 1;
+    }
+    initMusic(musics[musicIndex], singers[musicIndex]);
+}
+prev.addEventListener('click', prevMusic);
+
+
 function moveProgress() {
-  
+
 }
 
 playPause.addEventListener('click', togglePlay);
